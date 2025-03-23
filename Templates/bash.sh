8fea2b59adde9ef4be75f8714ce351e2eaf71c7a
Doncho Gunchev <doncho.gunchev@flyrlabs.com>
2025-03-23 17:52:19 +0200

    Add the Templates directory too.

---
+++ Templates/bash.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+shopt -s nullglob dotglob
+
+# if [ "$(uname -s)" != "Linux" ]; then echo "This script only works on Linux!" >&2; exit 254; fi
+# set -x # debug
+
+# if ! command -v ls &>/dev/null; then ...; fi
+# cd "$(dirname "$(readlink -f -- "${BASH_SOURCE[0]}")")" # mc highlight bug "
+# IFS= read -rd '' a < <(echo -ne "123\n\n\n456\n\n\n"); echo "$a"
+
+# Source directory even if it ends in new line. "${src%??}" drops the dot that guarded new lines and the last new line.
+# src=$(readlink -f -- "${BASH_SOURCE[0]}"; echo .); src="${src%??}"; src=$(dirname "$src"; echo .); src="${src%??}"
+src=$(readlink -f -- "${BASH_SOURCE[0]}"; echo .); src=${src%??}; src=$(dirname "$src"; echo .); src=${src%??}
+cd "$src"
+
+printf "We are in the %q directory.\n" "$src"
+
+LOCK_FILE='/tmp/global.lock'
+if ! (set -o noclobber; : > "$LOCK_FILE") 2>/dev/null; then
+    echo "Already running..." 2>&1
+    exit 1
+fi
+
+function cleanup {
+    echo "Cleaning up..."
+    trap - EXIT
+    rm -f "$LOCK_FILE"
+}
+
+trap cleanup EXIT
+
+USAGE="Usage:
+   $(basename "$0") SERVER [PORT/443]"
+
+HOST="prefix://${1:?"Error. You must supply a host name.
+
+${USAGE}"}"
+
+PORT="${2:-443}"
+
+echo "HOST=$HOST, PORT=$PORT."
+
+cleanup
+
+echo OK
---
+++ Templates/python.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# Add -sPE to previous line for more security in system-wide installation.
+# -*- coding: utf-8 -*-
+
+"""Template for ..."""
+
+import sys
+from typing import Optional, Union
+from shlex import split as shlex_split
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, RawDescriptionHelpFormatter
+
+
+class SmartFormatter(ArgumentDefaultsHelpFormatter, RawDescriptionHelpFormatter):
+    """Smarter help formatter
+
+    https://stackoverflow.com/questions/4375327 https://bugs.python.org/issue13041
+    https://bugs.python.org/issue12806 + https://pypi.org/project/argparse-formatter/
+    """
+
+    def __init__(self, prog, indent_increment=2, max_help_position=24, width=None):
+        try:
+            import shutil
+            width = shutil.get_terminal_size(fallback=(120, 32)).columns  # More reasonable nowadays @UndefinedVariable
+        except AttributeError:
+            width = 120
+        super(SmartFormatter, self).__init__(prog, indent_increment, max_help_position, width)
+
+
+class SmartArgumentParser(ArgumentParser):
+    """Extended ArgumentParser"""
+
+    def __init__(self, prog=None, usage=None, description=None, epilog=None, version=None, parents=[],
+                 formatter_class=SmartFormatter, prefix_chars='-+', fromfile_prefix_chars='@', argument_default=None,
+                 conflict_handler='error', add_help=True):
+        ArgumentParser.__init__(self, prog=prog, usage=usage, description=description, epilog=epilog, parents=parents,
+                                formatter_class=formatter_class, prefix_chars=prefix_chars,
+                                fromfile_prefix_chars=fromfile_prefix_chars, argument_default=argument_default,
+                                conflict_handler=conflict_handler, add_help=add_help)
+        if version is not None:
+            self.add_argument('--version', action='version', version='%(prog)s ' + version)
+
+    def convert_arg_line_to_args(self, arg_line):
+        """Split like shell, line by line, no EOL escaping"""
+        try:
+            return shlex_split(arg_line, comments=True)  # comments allowed
+        except ValueError:
+            return arg_line  # Best effort
+
+
+def main() -> Optional[Union[int, str]]:
+    """The main and only."""
+    print("Hello world!")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
